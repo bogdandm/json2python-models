d750cc1b491f7b0f369fd1154e108773a6d81cc4
bogdandm <bogdan.dm1995@yandex.ru>
2018-08-22 16:14:41 +0300

    Update PATH of test api dumps

--- testing_tools/real_apis/__init__.py
+++ testing_tools/real_apis/__init__.py
@@ -1,10 +1,12 @@
 import json
 from pathlib import Path
 
+BASE_PATH = (Path(__file__) / '..').resolve().absolute()
+
 
 def dump_response(app, name, data):
     # TODO: Make Path absolute
-    folder = Path(app)
+    folder = BASE_PATH / app
     folder.mkdir(exist_ok=True)
     with (folder / f"{name}.json").open("w") as f:
         json.dump(data, f, indent=4)
