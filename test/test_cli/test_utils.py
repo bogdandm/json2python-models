5f8f85dd223f0ea06090624ffd86cf80d43cd065
bogdandm <bogdan.dm1995@yandex.ru>
2018-11-12 19:30:35 +0300

    Fix test

--- test/test_cli/test_utils.py
+++ test/test_cli/test_utils.py
@@ -57,7 +57,7 @@ def test_convert_args(fn, value, expected):
 
 test_path_split_data = [
     pytest.param("./test/file.txt", ['.', 'test', 'file.txt'], id='base'),
-    pytest.param("X:\\test\\file.txt", ['X:\\', 'test', 'file.txt'], id='windows path'),
+    pytest.param("X:/test/file.txt", ['X:/', 'test', 'file.txt'], id='windows path'),
     pytest.param("/tmp/test/file.txt", ['/', 'tmp', 'test', 'file.txt'], id='unix path'),
     pytest.param("test/*.txt", ['test', '*.txt'], id='pattern'),
     pytest.param("test/**/*.txt", ['test', '**', '*.txt'], id='recursive pattern'),
