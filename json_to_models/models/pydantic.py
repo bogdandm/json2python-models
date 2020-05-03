3209fd32cfddad9c70297a60e41fdc8b5f1ea6e5
bogdan_dm <bogdan.dm1995@yandex.ru>
2020-05-03 13:42:42 +0300

    Pydantic: add tests

--- json_to_models/models/pydantic.py
+++ json_to_models/models/pydantic.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 from .base import GenericModelCodeGenerator, KWAGRS_TEMPLATE, sort_kwargs, template
 from ..dynamic_typing import DDict, DList, DOptional, ImportPathList, MetaData, ModelMeta
@@ -12,17 +12,14 @@ class PydanticModelCodeGenerator(GenericModelCodeGenerator):
     PYDANTIC_FIELD = template("Field({{ default }}{% if kwargs %}, KWAGRS_TEMPLATE{% endif %})"
                               .replace('KWAGRS_TEMPLATE', KWAGRS_TEMPLATE))
 
-    def __init__(self, model: ModelMeta, attrs_kwargs: dict = None,
-                 convert_unicode=True):
+    def __init__(self, model: ModelMeta, convert_unicode=True):
         """
         :param model: ModelMeta instance
         :param meta: Enable generation of metadata as attrib argument
         :param post_init_converters: Enable generation of type converters in __post_init__ methods
-        :param attrs_kwargs: kwargs for @attr.s() decorators
         :param kwargs:
         """
         super().__init__(model, post_init_converters=False, convert_unicode=convert_unicode)
-        self.attrs_kwargs = attrs_kwargs or {}
 
     def generate(self, nested_classes: List[str] = None, extra: str = "", **kwargs) \
             -> Tuple[ImportPathList, str]:
@@ -44,7 +41,7 @@ class PydanticModelCodeGenerator(GenericModelCodeGenerator):
         :return: imports, field data
         """
         imports, data = super().field_data(name, meta, optional)
-        default = '...'
+        default: Optional[str] = None
         body_kwargs = {}
         if optional:
             meta: DOptional
@@ -58,7 +55,10 @@ class PydanticModelCodeGenerator(GenericModelCodeGenerator):
         if name != data["name"]:
             body_kwargs["alias"] = f'"{name}"'
         if body_kwargs:
-            data["body"] = self.PYDANTIC_FIELD.render(default=default, kwargs=sort_kwargs(body_kwargs, DEFAULT_ORDER))
-        elif default != '...':
+            data["body"] = self.PYDANTIC_FIELD.render(
+                default=default or '...',
+                kwargs=sort_kwargs(body_kwargs, DEFAULT_ORDER)
+            )
+        elif default is not None:
             data["body"] = default
         return imports, data
